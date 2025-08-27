5d024c1e4bf83c78d2d22722f726753fda2e36d4
kemoagaben <bensimyapayzeka@gmail.com>
2025-08-27 15:50:16 +0300

    Create App.jsx

---
+++ public/src/src/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from './components/Dashboard';
+import AITOChat from './components/AITOChat';
+import EmotiveAvatar from './components/EmotiveAvatar';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-800 text-white">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
